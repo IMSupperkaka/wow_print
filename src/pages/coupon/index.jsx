eed2f5674a97d7fe5fc69c29cafbab803a2faaa6
fate <fatestylelove@outlook.com>
2020-08-27 16:47:52 +0800

    update

--- src/pages/coupon/index.jsx
+++ src/pages/coupon/index.jsx
@@ -4,6 +4,7 @@ import { View, ScrollView, Image, Button, Text } from '@tarojs/components'
 import './index.less'
 
 import waitpayPng from '../../../images/shop.jpg';
+import rightArrow from '../../../images/right_arrow@2x.png';
 
 class Index extends Component {
 
@@ -15,7 +16,12 @@ class Index extends Component {
     // }
 
     constructor() {
-        super(...arguments)
+        super(...arguments);
+        this.state = {
+            page: 0,
+            count: 0,
+            list: [1,2,3,4]
+        }
     }
 
     componentWillReceiveProps(nextProps) {
@@ -34,7 +40,7 @@ class Index extends Component {
     // onScrollToUpper = () => {}
 
     onScroll(e) {
-        console.log(e.detail)
+        // console.log(e.detail)
     }
 
     render() {
@@ -63,39 +69,41 @@ class Index extends Component {
                         <Text>温馨提示，每个订单只能使用一张优惠券哦～</Text>
                     </View>
                     <View className='list'>
-                        <View className='list-item'>
-                            <View className='list-item-header'>
-                                <View className="list-item-header-left">
-                                    <Image src={waitpayPng}/>
-                                    <View className="list-item-header-text">
-                                        <View className="name">6寸照片打印券10张</View>
-                                        <View>
-                                            <View className="sill">无门槛使用</View>
-                                            <View className="time">有效期至 2020.8.24</View>
-                                        </View>
-                                    </View>
-                                </View>
-                                <View className="list-item-header-btn">使用</View>
-                            </View>
-                            <View className="list-item-desc">
-                                <Text>适用6寸lomo卡，优惠券将在您结算时自动抵扣</Text>
-                            </View>
-                        </View>
-                        {/* <View className='list-item'>
-                            {
-                                [].map((item) => {
-                                    return (
+                        {
+                            this.state.list.map((item, index) => {
+                                return (
+                                    <View className='list-item'>
+                                        { 
+                                            index == 0 && 
+                                            <View className="top">
+                                                <View className="triangle"></View>
+                                                <Text className="new">新</Text>
+                                            </View> 
+                                        }
                                         <View className='list-item-header'>
                                             <View className="list-item-header-left">
                                                 <Image src={waitpayPng}/>
-                                                aaa
+                                                <View className="list-item-header-text">
+                                                    <View className="name">6寸照片打印券10张</View>
+                                                    <View>
+                                                        <View className="sill">无门槛使用</View>
+                                                        <View className="time">有效期至 2020.8.24</View>
+                                                    </View>
+                                                </View>
                                             </View>
                                             <View className="list-item-header-btn">使用</View>
                                         </View>
-                                    )
-                                })
-                            }
-                        </View> */}
+                                        <View className="list-item-desc">
+                                            <Text>适用6寸lomo卡，优惠券将在您结算时自动抵扣</Text>
+                                        </View>
+                                    </View>
+                                )
+                            })
+                        }
+                    </View>
+                    <View className="fotter">
+                         <Text>更多历史优惠券</Text>
+                         <Image src={rightArrow}/>
                     </View>
                 </ScrollView>
             </View>
