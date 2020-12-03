64706a55b27aaede6b33acf877a8daa4050c533a
celeste <celestew72@163.com>
2020-12-03 17:54:25 +0800

    fixbug

--- src/pages/orderDetail/index.jsx
+++ src/pages/orderDetail/index.jsx
@@ -10,6 +10,7 @@ import { orderStatus } from '../../utils/map/order'
 import address from '../../../images/icon_address@2x.png'
 import { getRouterParams } from '../../utils/utils'
 import Pay from '../../components/Pay'
+import day from 'dayjs';
 
 export default () => {
 
@@ -60,11 +61,11 @@ export default () => {
     })
 
     const createTimer = (expireTime) => {
-        let closeTime = new Date(expireTime).getTime();
-        let currentTime = new Date().getTime();
+        let closeTime = day(expireTime).valueOf();
+        let currentTime = day().valueOf();
         setCountDown(turnHMS(closeTime - currentTime));
         timer.current = setInterval(() => {
-            currentTime = new Date().getTime();
+            currentTime = day().valueOf();
             let timeSub = closeTime - currentTime
             if(timeSub > 0) {
                 setCountDown(turnHMS(timeSub));
@@ -73,7 +74,6 @@ export default () => {
     }
 
     useEffect(() => {
-        console.log(countDown)
         if(countDown === '00:00:00') {
             clearInterval(timer.current)
         }
@@ -83,9 +83,9 @@ export default () => {
     // 时间戳差值转换时分秒
     const turnHMS = (time) => {
         let hms = time / 1000;
-        let hour = parseInt(hms / (60 * 60), 10)
-        let minutes = parseInt((hms % (60 * 60)) / 60, 10) 
-        let seconds = parseInt((hms % (60 * 60)) % 60, 10)
+        let hour = parseInt(hms / (60 * 60), 10);
+        let minutes = parseInt((hms % (60 * 60)) / 60, 10);
+        let seconds = parseInt((hms % (60 * 60)) % 60, 10);
         hms = `${hour.toString().length == 2 ? hour : '0' + hour}:${minutes.toString().length == 2 ? minutes : '0' + minutes}:${seconds.toString().length == 2 ? seconds : '0' + seconds}`
         return hms
     }
