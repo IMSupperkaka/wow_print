60c8ebefe71d84763035ce1dcd44e43a53db5b22
IMSuperkaka <1072815089@qq.com>
2020-12-07 20:03:57 +0800

    fixbug

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,12 +1,15 @@
+
 export const sessionStorage = {
     setItem () {
         if (process.env.TARO_ENV === 'h5') {
             return window.sessionStorage.setItem(...arguments);
         }
+        return null;
     },
     getItem() {
         if (process.env.TARO_ENV === 'h5') {
             return window.sessionStorage.getItem(...arguments);
         }
+        return null;
     }
 }
