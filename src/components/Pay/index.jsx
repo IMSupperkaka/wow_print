1d72dec52cddd94bfd5806b1d327637a8d0f6bd6
IMSuperkaka <1072815089@qq.com>
2020-12-08 10:41:16 +0800

    fixbug

--- src/components/Pay/index.jsx
+++ src/components/Pay/index.jsx
@@ -160,8 +160,7 @@ const usePay = (props) => {
                 ]
                 const redirect_url = `${location.protocol}//${location.host}/pages/orderDetail/index?id=${response.loanId}&save_dva=${encodeURIComponent(JSON.stringify(saveDva))}`
                 const submitUrl = payData.mweb_url + `&redirect_url=${encodeURIComponent(redirect_url)}`;
-                console.log(redirect_url)
-                // formSubmit(submitUrl);
+                formSubmit(submitUrl);
             }
         }
     }
