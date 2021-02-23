504e18948104e74cbdb2a0f2553f468d92ec50b8
celeste <celestew72@163.com>
2021-02-23 11:53:29 +0800

    fix:pillowmodel

--- src/package-main-order/pages/stageView/pillowmodel.js
+++ src/package-main-order/pages/stageView/pillowmodel.js
@@ -4,7 +4,6 @@ const fileInfo = {
     x: 40,
     y: 45
 }
-
 export default [
     {
         name: "原图定制",
@@ -13,7 +12,9 @@ export default [
             height: 610,
             bgPath: 'http://cdn.91jiekuan.com/Fj2pQO9GW2THiGt5xwzdAxwE0apc',
             bgCover: false,
-            maskPath: 'https://cdn.91jiekuan.com/FklfQEBnJcVcbu0IneZvZyZlJhjY',
+            mixBlendMode: 'multiply',
+            maskPath: 'http://cdn.91jiekuan.com/FgRBoe84ktW7DBKz93Z5P1sheLPr',
+            topDecoration: 'http://cdn.91jiekuan.com/FhRPq0wRBr4ivgPfbW33uhw2B9em',
             thumbnail: 'http://cdn.91jiekuan.com/Fru-pa8Jk2dyXIYhbM5pvxLHZPSY'
         },
         fileInfo: {
@@ -21,10 +22,10 @@ export default [
         },
         editArea: [
             {
-                x: 0,
-                y: 0,
+                x: 48,
+                y: 8,
                 width: 560,
-                height: 560
+                height: 560,
             }
         ]
     }
